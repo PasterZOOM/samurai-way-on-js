138fcccc448f23cb6f5159d984495cbf60a791b3
Ilya Pasternak <pasterzoom@gmail.com>
2022-05-23 12:09:08 +0300

    52 - Side Effect

--- src/components/Body/Users/Users.jsx
+++ src/components/Body/Users/Users.jsx
@@ -4,14 +4,19 @@ import axios from "axios";
 import userPhoto from './img/userPhoto.png'
 
 const Users = (props) => {
-    if (props.users.length === 0) {
-        axios.get('https://social-network.samuraijs.com/api/1.0/users')
-            .then(response => {
-                props.setUsers(response.data.items)
-            })
+
+    const getUsers = () => {
+        if (props.users.length === 0) {
+            axios.get('https://social-network.samuraijs.com/api/1.0/users')
+                .then(response => {
+                    props.setUsers(response.data.items)
+                })
+        }
     }
+
     return (
         <div>
+            <button onClick={getUsers}>get users</button>
             {
                 props.users.map(u => <div key={u.id}>
                     <span>
