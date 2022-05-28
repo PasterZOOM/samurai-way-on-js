eb407eb4a346749a7a0a17e8c942940808ebfd06
Ilya Pasternak <pasterzoom@gmail.com>
2022-05-28 14:45:33 +0300

    55 - pagination, постраничный вывод пользователей

--- src/components/Body/Users/Users.jsx
+++ src/components/Body/Users/Users.jsx
@@ -6,17 +6,43 @@ import userPhoto from './img/userPhoto.png'
 class Users extends React.Component {
 
     componentDidMount() {
-        axios.get('https://social-network.samuraijs.com/api/1.0/users')
+        axios.get(
+            `https://social-network.samuraijs.com/api/1.0/users?count=${this.props.pageSize}&page=${this.props.currentPage}`)
+            .then(response => {
+                this.props.setUsers(response.data.items)
+                this.props.setTotalUsersCount(response.data.totalCount)
+            })
+    }
+
+    onPageChanged = (pageNumber) => {
+        this.props.setCurrentPage(pageNumber)
+        axios.get(
+            `https://social-network.samuraijs.com/api/1.0/users?count=${this.props.pageSize}&page=${pageNumber}`)
             .then(response => {
                 this.props.setUsers(response.data.items)
             })
     }
 
     render() {
+        let pages = []
+        if (this.props.currentPage<=3) {
+            for (let i = 1; i <= 7; i++) {
+                pages.push(i)
+            }
+        } else {
+            for (let i = (this.props.currentPage-3); i <= (this.props.currentPage+3); i++) {
+                pages.push(i)
+            }
+        }
         return (
             <div>
-                {
-                    this.props.users.map(u => <div key={u.id}>
+                {pages.map(p =>
+                    <span className={this.props.currentPage === p && s.selectedPage}
+                          onClick={() => this.onPageChanged(p)}>
+                        {p}
+                    </span>
+                )}
+                {this.props.users.map(u => <div key={u.id}>
                     <span>
                             <img src={u.photos.small !== null ? u.photos.small : userPhoto}
                                  className={s.userPhoto}
@@ -27,7 +53,7 @@ class Users extends React.Component {
                                 : <button onClick={() => this.props.follow(u.id)}>Follow</button>}
                         </div>
                     </span>
-                        <span>
+                    <span>
                         <span>
                             <div>{u.name}</div>
                             <div>{u.status}</div>
@@ -37,7 +63,7 @@ class Users extends React.Component {
                             <div>{'u.location.city'}</div>
                         </span>
                     </span>
-                    </div>)
+                </div>)
                 }
             </div>
         )
