90306e7493f829108f59505a5b07c2ffe8260193
Ilya Pasternak <pasterzoom@gmail.com>
2022-05-28 18:22:52 +0300

    57 - пример Preloader, loader-gif, isFetching indication

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import s from "./Header.module.css";
+import logo from '../../assets/images/HeadLogo.png'
 
 const Header = () => {
     return (
         <header className={s.header}>
             <img
-                src='https://p7.hiclipart.com/preview/209/755/790/starbucks-cafe-coffee-logo-restaurant-starbucks.jpg' alt='logo'/>
+                src={logo} alt='logo'/>
         </header>)
 }
 
